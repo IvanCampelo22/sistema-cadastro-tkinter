92eeb61460e5cf359baaa5885cc4f47829d18378
Ivan Campelo <ivanilsondasilva1973@gmail.com>
2022-01-11 08:27:20 -0300

    começando para valer

--- main.py
+++ main.py
@@ -5,21 +5,33 @@ class Application:
     def __init__(self, master=None):
         self.fontePadrao = ("Arial", "10")
         self.primeiroConteiner = Frame(master)
-        self.primeiroConteiner["pady"] = "10"
+        self.primeiroConteiner["pady"] = "30"
         self.primeiroConteiner.pack()
 
-        self.titulo = Button(self.primeiroConteiner)
-        self.titulo["text"] = "clique aqui"
-        self.titulo["font"] = ("Calibri", "8")
-        self.titulo["width"] = 12
-        self.titulo["command"] = self.print_hy()
+        self.titulo = Label(self.primeiroConteiner, text="Sistema de Cadastro", font=self.fontePadrao)
         self.titulo.pack(side=TOP)
+
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = "10"
+        self.segundoContainer.pack()
+
+        self.nome = Label(self.segundoContainer, text="Nome: ", font=self.fontePadrao)
+        self.nome.pack(side=LEFT)
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = "10"
+        self.terceiroContainer.pack(side=LEFT)
+
+        self.nome = Entry(self.terceiroContainer)
+        self.nome["width"] = 30
+        self.nome["font"] = self.fontePadrao
+        self.nome.pack(side=LEFT)
 
         self.mensagem = Label(self.primeiroConteiner, text="", font=self.fontePadrao)
         self.mensagem.pack()
 
     def print_hy(self):
-
+        self.mensagem["text"] = "Olá"
 
 
 root = Tk()
